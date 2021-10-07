6d31e2ab133f2f5e653a5f9e8f5e984bdbd78dff
Lost <ma810115@stust.edu.tw>
2021-10-07 23:38:39 +0800

    A

---
+++ .nuxt/components/index.js
@@ -0,0 +1,3 @@
+export { default as CSlider } from '../..\\components\\c-slider.vue'
+
+export const LazyCSlider = import('../..\\components\\c-slider.vue' /* webpackChunkName: "components_c-slider" */).then(c => c.default || c)
---
+++ .nuxt/router.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL } from '@nuxt/ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _94dd5566 = () => interopDefault(import('..\\pages\\music.vue' /* webpackChunkName: "pages/music" */))
+const _6e2e717a = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+
+// TODO: remove in Nuxt 3
+const emptyFn = () => {}
+const originalPush = Router.prototype.push
+Router.prototype.push = function push (location, onComplete = emptyFn, onAbort) {
+  return originalPush.call(this, location, onComplete, onAbort)
+}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/tsmcdododo.github.io/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/music",
+    component: _94dd5566,
+    name: "music"
+  }, {
+    path: "/",
+    component: _6e2e717a,
+    name: "index"
+  }],
+
+  fallback: false
+}
+
+function decodeObj(obj) {
+  for (const key in obj) {
+    if (typeof obj[key] === 'string') {
+      obj[key] = decodeURIComponent(obj[key])
+    }
+  }
+}
+
+export function createRouter () {
+  const router = new Router(routerOptions)
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    const r = resolve(to, current, append)
+    if (r && r.resolved && r.resolved.query) {
+      decodeObj(r.resolved.query)
+    }
+    return r
+  }
+
+  return router
+}
