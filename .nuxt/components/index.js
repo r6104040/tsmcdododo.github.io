ce006cca3beb5399193d124f82dd15eeaa6aec6a
Lost <ma810115@stust.edu.tw>
2021-10-11 00:58:17 +0800

    V1
    haha

--- .nuxt/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as CSlider } from '../..\\components\\c-slider.vue'
-
-export const LazyCSlider = import('../..\\components\\c-slider.vue' /* webpackChunkName: "components_c-slider" */).then(c => c.default || c)
